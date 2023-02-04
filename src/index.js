ce070c50ee3c14dac24f047f41934c5c13ca872d
Fishviche <evalerianom@gmail.com>
2023-02-04 13:28:08 +0000

    adding error handler

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const routes = require('./routes');
-const { errorHandler } = require('./controllers/report.controller.js')
+const { errorHandler } = require('./middlewares/error.handler.js')
 // Config
 app.set('port', process.env.PORT || 3001);
 
@@ -10,10 +10,12 @@ app.set('port', process.env.PORT || 3001);
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors());
-app.use(errorHandler)
+
 // Routes
 app.use(routes);
 
+//  Handle error
+app.use(errorHandler);
 // Start server
 app.listen(app.get('port'), () => {
   console.log('Server on port: ', app.get('port'));
