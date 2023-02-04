ba130fd7a92ce1b98c2d9a49a6b95c1e7fa29ad0
Fishviche <evalerianom@gmail.com>
2023-02-04 13:28:44 +0000

    db configuration

--- src/database/database.js
+++ src/database/database.js
@@ -2,8 +2,5 @@ const { Pool } = require('pg');
 const { db } = require('../config/config.js');
 const pool = new Pool(db);
 module.exports = {
-  async query(text, params){
-    const res = await pool.query(text, params);
-    return res;
-  }
+  pool
 }
