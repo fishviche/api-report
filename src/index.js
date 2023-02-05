2703fa9c9b7f762a9bc52edde110dc9750cc7f42
Fishviche <evalerianom@gmail.com>
2023-02-05 19:15:47 +0000

    adding bodyparser to mange form-data

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ const cors = require('cors');
 const app = express();
 const routes = require('./routes');
 const { errorHandler } = require('./middlewares/error.handler.js')
+const bodyParser = require('body-parser')
 // Config
 app.set('port', process.env.PORT || 3001);
 
 // Middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // Routes
 app.use(routes);
