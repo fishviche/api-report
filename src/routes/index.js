5110651da989bc621515949af9aaee43d02dc8ef
Fishviche <evalerianom@gmail.com>
2023-02-05 19:16:13 +0000

    adding routing

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const reportRoute = require('./report.routes');
+const categoryRoute = require('./category.routes');
+const reporterRoute = require('./reporter.routes');
 
 const router = express.Router();
 
 router.use('/report', reportRoute);
+router.use('/category', categoryRoute);
+router.use('/reporter', reporterRoute);
 
 module.exports = router;
