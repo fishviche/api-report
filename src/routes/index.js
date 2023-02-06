b30b5a713b5b693656bcc32b1470dc3a765a93c1
Fishviche <evalerianom@gmail.com>
2023-02-06 03:00:21 +0000

    adding location API

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const reportRoute = require('./report.routes');
 const categoryRoute = require('./category.routes');
 const reporterRoute = require('./reporter.routes');
+const locationRoute = require('./location.routes');
 
 const router = express.Router();
 
 router.use('/report', reportRoute);
 router.use('/category', categoryRoute);
 router.use('/reporter', reporterRoute);
+router.use('/location', locationRoute);
 
 module.exports = router;
